464ab5ba31ed45118b38f96e14d715398b471f3d
Travis Tang <59107903+travistangvh@users.noreply.github.com>
2023-04-10 10:48:45 +0800

    exploring how connection will be made

--- bin/predictStream.py
+++ bin/predictStream.py
@@ -7,11 +7,13 @@ import torch
 from torch import nn
 from models import MyCNN
 from utils import get_global_config, compute_batch_accuracy, acked, get_producer_config, get_consumer_config, build_spark_session, get_waveform_path, create_batch, get_arr, get_base_time, get_ending_time,get_record,run_model,run_model_dummy
-from pyspark.sql.functions import explode, split, from_json, to_json, col, struct
+from pyspark.sql.functions import explode, split, from_json, to_json, col, struct, trim,expr
 import pyspark.pandas as ps
 import mysql.connector
 import datetime
 import pandas as pd
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, FloatType, ArrayType
+from pyspark.sql.functions import lit
 
 cfg = get_global_config()
 cnx = mysql.connector.connect(user='root', 
@@ -21,13 +23,113 @@ cnx = mysql.connector.connect(user='root',
 
 # Define a function to write each batch of streaming data to MySQL
 def write_to_mysql(batch_df, batch_id):
+
+    # Wait until enough data points has arrived.
+    import time 
+    start_time = time.time()
+    print(f"The number of rows of data: {len(batch_df.head(1))}")
+    # Set the timeout for waiting for data
+    timeout = 60  # Wait for 60 seconds
+
+    # while len(batch_df.head(1)) < 120:  # Wait until at least 10 rows have arrived
+    #     time.sleep(1)
+    #     if time.time() - start_time > timeout:
+    #         # If the timeout is exceeded, break out of the loop and return
+    #         break
+    #     time.sleep(1)  # Wait for 1 second before checking again
+    #     print("Not enough data has arrived.")
+
+    # if len(batch_df.head(1)) >= 120:
+    print("Enough data has arrived!")
+
+    batch_df_collected = batch_df.collect()
+    rows = list(batch_df_collected)
+    print(rows)
+    
+    # Retrieve the "value" column from batch_df
+    batch_df = batch_df.select("key", "value")
+
+    # select the value columns and convert it to a pandas dataframe
+    # batch_df = batch_df.select("value").toPandas()
+
+    # print(batch_df)
+
+    # what's inside the value: ("CAST(value as STRING) AS a", "timestamp", "1 as ID")
+    value_schema = StructType([
+        StructField("a", StringType()),
+        StructField("ID", IntegerType()),
+        StructField("structs", StructType())
+    ])
+
+    # Convert the "structs" column from a json string to a column of structs
+    struct_schema = StructType([
+        StructField("HR", ArrayType(FloatType())), # this is NOT a float type or an INT type! 
+        StructField("RESP", ArrayType(FloatType())),
+        StructField("PULSE", ArrayType(FloatType())),
+        StructField("etco2", ArrayType(FloatType())),
+        StructField("CVP", ArrayType(FloatType())),
+        StructField("AWRR", ArrayType(FloatType())),
+        StructField("NBP_Mean", ArrayType(FloatType())),
+        StructField("NBP_Dias", ArrayType(FloatType())),
+        StructField("NBP_Sys", ArrayType(FloatType()))
+        ])
+
+    batch_df_collected = batch_df.select("value").collect()
+    rows = list(batch_df_collected)
+    print(rows)
+
+    parsed_df = batch_df \
+    .select(from_json(col("value").cast("string"), struct_schema).alias("structs")) 
+
+
+    parsed_df_collected = parsed_df.collect()
+    rows = list(parsed_df_collected)
+    print(rows)
+
+
+    # parsed_df = parsed_df.select("structs.a", "parsed_value.id", "parsed_value.structs") \
+    # .withColumn("structs_a", from_json(col("structs").cast("string"), struct_schema))
+    
+    # parsed_df_collected = parsed_df.collect()
+    # rows = list(parsed_df_collected)
+    # print(rows)
+
+    parsed_df = parsed_df.select("structs.HR", "structs.RESP", "structs.PULSE", "structs.etco2", "structs.CVP", "structs.AWRR", "structs.NBP_Mean", "structs.NBP_Dias", "structs.NBP_Sys")
+
+
+    # parsed_df = parsed_df \
+    # .select("HR", col("structs.HR")) \
+    # .withColumn("RESP", expr("trim(BOTH '\"' FROM structs_a.RESP)").cast(FloatType())) \
+    # .withColumn("PULSE", expr("trim(BOTH '\"' FROM structs_a.PULSE)").cast(FloatType())) 
+        
+    # .withColumn("field1", trim(parsed_df["structs_a.field1"],'"').cast(IntegerType())) \
+    # .withColumn("field2", trim(parsed_df["structs_a.field2"],'"').cast(IntegerType())) \
+    # .withColumn("field3", trim(parsed_df["structs_a.field3"],'"').cast(IntegerType()))
+
+    print('yoohoo')
+    print(parsed_df)
+    parsed_df_collected = parsed_df.select("HR","RESP").collect()
+    rows = list(parsed_df_collected)
+    print(rows)
+
+    # # Convert the JSON column to a struct column
+    # struct_col = from_json(batch_df["structs"], struct_schema)
+
+    # # Select the struct column and convert it to a numpy array
+    # numpy_array = np.array(batch_df.select(struct_col).collect())
+
+    # print(numpy_array)
+
     cursor = cnx.cursor()
 
+    # Call ML
+
     # Convert the batch DataFrame to a list of tuples
-    data = [tuple(row) for row in batch_df.collect()]
+    data = [tuple(row) for row in parsed_df_collected]
 
     # Construct the SQL query to insert the data into MySQL
-    query = "INSERT INTO mytable (col1, col2) VALUES (%s, %s)"
+    query = "INSERT INTO mytable (col1, col2) VALUES (3484, 3092)"
+    # query = "INSERT INTO mytable (col1, col2) VALUES (%s, %s)"
 
     # Insert the data into MySQL using a prepared statement
     cursor.executemany(query, data)
@@ -55,23 +157,6 @@ def main():
     producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
     del producer_conf['bootstrap.servers']
 
-    def preprocess_and_send_to_kafka(batch_df, batch_id):
-        """The function is fed into foreachBatch of a writestream.
-        It preprocesses the data and sends it to Kafka.
-        Thus it should be modified for necessary preprocessing"""
-
-        # Preprocess the data
-        # Here it does nothing.
-        preprocessed_df = batch_df #.select(lower(col("value")).alias("value_lower"))
-        
-        # Send the preprocessed data to Kafka
-        preprocessed_df.write.format("kafka")\
-            .options(**producer_conf)\
-            .option("topic", "response-stream")\
-            .option("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .option("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .save()
-
     def msg_process(server, topic):
         """Create a streaming dataframe that takes in values of messages received, 
         together with the current timestamp.
@@ -79,6 +164,7 @@ def main():
         Then, process the message in batch
         Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
 
+        print(f"Reading from {topic}!")
         df = (spark.
                 readStream
                 .format("kafka")
@@ -89,13 +175,24 @@ def main():
             )
         
         # Select the value and timestamp (the message is received)
-        base_df = df.selectExpr("CAST(value as STRING)", "timestamp")
-        
+        #  DataFrame[key: binary, value: binary, topic: string, partition: int, offset: bigint, timestamp: timestamp, timestampType: int]
+        df = df.selectExpr("CAST(key as STRING)" ,"CAST(value as STRING)")
 
+        # df_collected = df.select("value").collect()
+        # rows = list(df_collected)
+        # print(rows)
+
+        # Write a UDF that applies data to one mini batch at one time
+        # The mini batch is a list of tuples, where each tuple is a message and its timestamp.
         # The model needs to be called to store the data
         model = torch.load(cfg['MODELPATH'])
         device = torch.device("cuda" if cfg['USE_CUDA'] and torch.cuda.is_available() else "cpu")
+        print('yoohoo im here')
+        # print(base_df)
 
+        # Select one mini batch at one time. 
+        # The mini batch is a list of tuples, where each tuple is a message and its timestamp.
+    
         # Once we manage to set up the actual data, 
         # we will pull data from Kafka stream instead of having to retrieve 
         # data from local.
@@ -112,15 +209,10 @@ def main():
         # query = base_df.writeStream.outputMode("append").format("console").trigger(processingTime='10 seconds').start()
         # query.awaitTermination()
 
-        ## Write the preprocessed DataFrame to Kafka in batches.
-        # kafka_writer: DataStreamWriter = base_df.writeStream.foreachBatch(preprocess_and_send_to_kafka)
-        # kafka_query: StreamingQuery = kafka_writer.start()
-        # kafka_query.awaitTermination()
-
         # Write the streaming data to MySQL using foreachBatch
-        query = base_df.writeStream.foreachBatch(write_to_mysql).trigger(processingTime='10 seconds').start()
+        query = df.writeStream.foreachBatch(write_to_mysql).trigger(processingTime='10 seconds').start()
         # query.awaitTermination()
-        
+
         for i in range(10):
             print("Model has successfully run!")
             print(y_pred)
@@ -137,9 +229,8 @@ def main():
         print('Rows from mytable:')
         for idx, row in enumerate(rows1):
             # Too verbose
-            if idx %50==0:
+            if idx%50==0:
                 print(row)
-            idx+=1
         
         query.awaitTermination()
 
--- bin/processStream.py
+++ bin/processStream.py
@@ -2,90 +2,188 @@
 
 import argparse
 from confluent_kafka import Consumer, Producer
-from pyspark.sql.functions import explode, split, from_json, to_json, col, struct, lower
+from pyspark.sql.functions import explode, split, from_json, to_json, col, struct, lower, array
 from pyspark.sql.streaming import DataStreamWriter, StreamingQuery
-from utils import acked, get_consumer_config, get_producer_config, build_spark_session
-
+from utils import acked, get_consumer_config, get_producer_config, build_spark_session, get_global_config
+from pyspark.sql.functions import lit
+from pyspark.sql.functions import concat, to_json, struct, lit, col
 def main():
-    """Create SparkSession.
-    Explanation on why the .config(spark.jars.packages) is needed: 
-    https://stackoverflow.com/questions/72812187/pythonfailed-to-find-data-source-kafkav"""
-    spark = build_spark_session()
+	"""Create SparkSession.
+	Explanation on why the .config(spark.jars.packages) is needed: 
+	https://stackoverflow.com/questions/72812187/pythonfailed-to-find-data-source-kafkav"""
+	spark = build_spark_session()
+	cfg = get_global_config()
 
-    """Get arguments from command line"""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--preprocessing-topic', 
-                        type=str,
-                        help='Name of the Kafka topic to receive unprocessed data.')
-    parser.add_argument('--model-call-topic', 
-                        type=str, 
-                        help='Name of the Kafka topic to send preprocessed data to machine learning model.')
-    parser.add_argument('--model-response-topic', 
-                        type=str, 
-                        help='Name of the Kafka topic to receive response from machine learning model.')
+	"""Get arguments from command line"""
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument('--preprocessing-topic', 
+						type=str,
+						help='Name of the Kafka topic to receive unprocessed data.')
+	parser.add_argument('--model-call-topic', 
+						type=str, 
+						help='Name of the Kafka topic to send preprocessed data to machine learning model.')
+	parser.add_argument('--model-response-topic', 
+						type=str, 
+						help='Name of the Kafka topic to receive response from machine learning model.')
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    """Create producer and consumer and interact with kafka"""
-    # Define Kafka consumer configuration
-    consumer_conf = get_consumer_config()
+	"""Create producer and consumer and interact with kafka"""
+	# Define Kafka consumer configuration
+	consumer_conf = get_consumer_config()
 
-    # Define Kafka producer configuration
-    producer_conf = get_producer_config()
-    producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
-    del producer_conf['bootstrap.servers']
+	# Define Kafka producer configuration
+	producer_conf = get_producer_config()
+	producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
+	del producer_conf['bootstrap.servers']
 
-    # Define the function to preprocess and send the data to Kafka
-    def preprocess_and_send_to_kafka(batch_df, batch_id):
-        """The function is fed into foreachBatch of a writestream.
-        It preprocesses the data and sends it to Kafka.
-        Thus it should be modified for necessary preprocessing"""
 
-        # Preprocess the data
-        # Here it does nothing.
-        preprocessed_df = batch_df #.select(lower(col("value")).alias("value_lower"))
-        
-        # Send the preprocessed data to Kafka
-        preprocessed_df.write.format("kafka")\
-            .options(**producer_conf)\
-            .option("topic", "call-stream")\
-            .option("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .option("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .save()
+	# mock_df = spark.read.csv(F'file://{cfg["EXPLOREPATH"]}/X.TESTINPUT', header=True, inferSchema=True)
+	# new_col = to_json(struct(
+	# 			array(mock_df['HR']),
+	# 			array(mock_df['RESP']),
+	# 			array(mock_df['PULSE']),
+	# 			array(mock_df['etco2']),
+	# 			array(mock_df['SpO2']),
+	# 			array(mock_df['CVP']),
+	# 			array(mock_df['AWRR']),
+	# 			array(mock_df['NBP_Mean']),
+	# 			array(mock_df['NBP_Dias']),
+	# 			array(mock_df['NBP_Sys'])
+	# 		))
+	# new_col = to_json(struct(
+	# 			array(mock_df['value']['HR']),
+	# 			array(mock_df['value']['RESP']),
+	# 			array(mock_df['value']['PULSE']),
+	# 			array(mock_df['value']['etco2']),
+	# 			array(mock_df['value']['SpO2']),
+	# 			array(mock_df['value']['CVP']),
+	# 			array(mock_df['value']['AWRR']),
+	# 			array(mock_df['value']['NBP_Mean']),
+	# 			array(mock_df['value']['NBP_Dias']),
+	# 			array(mock_df['value']['NBP_Sys'])
+	# 		))
+	# Define the function to preprocess and send the data to Kafka
+	def preprocess_and_send_to_kafka(batch_df, batch_id):
+		"""The function is fed into foreachBatch of a writestream.
+		It preprocesses the data and sends it to Kafka.
+		Thus it should be modified for necessary preprocessing"""
 
-    def msg_process(server, topic):
-        """Create a streaming dataframe that takes in values of messages received, 
-        together with the current timestamp.
-        Then, print them out.
-        Then, process the message in batch
-        Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
+		# Preprocess the data
+		# Here it does nothing.
+		preprocessed_df = batch_df #.select(lower(col("value")).alias("value_lower"))
 
-        df = (spark.
-                readStream
-                .format("kafka")
-                .option("kafka.bootstrap.servers", server)
-                .option("subscribe", topic)
-                .option("startingOffsets", "latest")
-                .load()
-            )
-        
-        # Select the value and timestamp (the message is received)
-        base_df = df.selectExpr("CAST(value as STRING)", "timestamp")
-        
-        # to see what "base_df" is like in the stream,
-        # Uncomment base_df.writeStream.outputMode(...)
-        # and comment out base_df.writeStream.foreachBatch(...)
-        # query = base_df.writeStream.outputMode("append").format("console").trigger(processingTime='10 seconds').start()
-        # query.await_termination()
+		# package the "value", "timestamp" and "ID" into one array 
+		# and send it to Kafka
+		# preprocessed_df = preprocessed_df.select(
+		#     to_json(struct(
+		#         lit(124),
+		#         lit(256),
+		#         lit(358)
+		#     )).alias("structs")
+		# )
 
-        # Write the preprocessed DataFrame to Kafka in batches.
-        kafka_writer: DataStreamWriter = base_df.writeStream.foreachBatch(preprocess_and_send_to_kafka)
-        kafka_query: StreamingQuery = kafka_writer.start()
-        print("await termination")
-        kafka_query.awaitTermination()
+		# This is the mock data from one patient
 
-    msg_process(consumer_conf['bootstrap.servers'], 
-                args.preprocessing_topic)
+		# Convert data in mock_df to a column in preprocessed_Df, which is a column called struct 
+		# the struct contains 10 columns, one for each column of mock_df.
+		# all the values of each column is stored as an array
+
+		
+		# preprocessed_df = preprocessed_df.withColumn("structs",
+		# 	to_json(struct(
+		# 		array(lit(124), lit(124)).alias("HR"),
+		# 		array(lit(124), lit(124)).alias("RESP"),
+		# 		array(lit(124), lit(124)).alias("PULSE"),
+		# 		array(lit(124), lit(124)).alias("etco2"),
+		# 		array(lit(124), lit(124)).alias("SpO2"),
+		# 		array(lit(124), lit(124)).alias("CVP"),
+		# 		array(lit(124), lit(124)).alias("AWRR"),
+		# 		array(lit(124), lit(124)).alias("NBP_Mean"),
+		# 		array(lit(124), lit(124)).alias("NBP_Dias"),
+		# 		array(lit(124), lit(124)).alias("NBP_Sys")
+		# 	)
+		# 	))
+		# preprocessed_df = preprocessed_df.withColumn(
+		# 	'structs',
+		# 	to_json(struct(
+		# 		array(mock_df['0']).alias("HR"),
+		# 		array(mock_df['1']).alias("RESP"),
+		# 		array(mock_df['2']).alias("PULSE"),
+		# 		array(mock_df['3']).alias("etco2"),
+		# 		array(mock_df['4']).alias("SpO2"),
+		# 		array(mock_df['5']).alias("CVP"),
+		# 		array(mock_df['6']).alias("AWRR"),
+		# 		array(mock_df['7']).alias("NBP_Mean"),
+		# 		array(mock_df['8']).alias("NBP_Dias"),
+		# 		array(mock_df['9']).alias("NBP_Sys")
+		# 	))
+		# )
+		# hard coding some random values
+		preprocessed_df = preprocessed_df.withColumn(
+			'structs',
+			to_json(struct(
+				array([lit(95.0) for _ in range(120)]).alias("HR"),
+				array([lit(95.0) for _ in range(120)]).alias("RESP"),
+				array([lit(95.0) for _ in range(120)]).alias("PULSE"),
+				array([lit(95.0) for _ in range(120)]).alias("etco2"),
+				array([lit(95.0) for _ in range(120)]).alias("SpO2"),
+				array([lit(95.0) for _ in range(120)]).alias("CVP"),
+				array([lit(95.0) for _ in range(120)]).alias("AWRR"),
+				array([lit(95.0) for _ in range(120)]).alias("NBP_Mean"),
+				array([lit(95.0) for _ in range(120)]).alias("NBP_Dias"),
+				array([lit(95.0) for _ in range(120)]).alias("NBP_Sys")
+			)))
+		#add a column "patient_ID" to the dataframe with a fixed value of 1
+		preprocessed_df = preprocessed_df.withColumn("patient_ID", lit(2659))
+		
+		# Send the preprocessed data to Kafka
+		preprocessed_df.selectExpr(
+        "CAST(patient_ID AS STRING) AS key",
+        "structs AS value" # oh my, this took me so long. you have to specify "structs" as the value of the key "value". otherwise it will send the "value" column 
+    ).write.format("kafka")\
+			.options(**producer_conf)\
+			.option("topic", "call-stream")\
+			.option("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
+			.option("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
+			.option("key", "patient_ID")\
+			.save()
+		
+		# to do: write to the same topic but different ID for each patient ID.
+	def msg_process(server, topic):
+		"""Create a streaming dataframe that takes in values of messages received, 
+		together with the current timestamp.
+		Then, print them out.
+		Then, process the message in batch
+		Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
+
+		df = (spark.
+				readStream
+				.format("kafka")
+				.option("kafka.bootstrap.servers", server)
+				.option("subscribe", topic)
+				.option("startingOffsets", "latest")
+				.load()
+			)
+		
+		# Select the value and timestamp (the message is received)
+		# base_df = df.selectExpr("CAST(value as STRING) AS a", "timestamp", "1 as ID") # This will throw error!
+		base_df = df.selectExpr("CAST(value as STRING)", "timestamp")
+		
+		# to see what "base_df" is like in the stream,
+		# Uncomment base_df.writeStream.outputMode(...)
+		# and comment out base_df.writeStream.foreachBatch(...)
+		# query = base_df.writeStream.outputMode("append").format("console").trigger(processingTime='10 seconds').start()
+		# query.await_termination()
+
+		# Write the preprocessed DataFrame to Kafka in batches.
+		kafka_writer: DataStreamWriter = base_df.writeStream.foreachBatch(preprocess_and_send_to_kafka)
+		kafka_query: StreamingQuery = kafka_writer.start()
+		print("await termination")
+		kafka_query.awaitTermination()
+
+	msg_process(consumer_conf['bootstrap.servers'], 
+				args.preprocessing_topic)
 
 if __name__ == "__main__":
-    main()
+	main()
