6a7aa1558b7afdd3522cccb209ce28960fef5cb1
Travis Tang <59107903+travistangvh@users.noreply.github.com>
2023-04-10 17:41:30 +0800

    edit predictStream the second time

--- bin/predictStream.py
+++ bin/predictStream.py
@@ -12,142 +12,142 @@ import pyspark.pandas as ps
 import mysql.connector
 import datetime
 import pandas as pd
+from pyspark.sql.functions import explode, split, from_json, to_json, col, struct, window, avg, collect_list,first
 
 cfg = get_global_config()
 cnx = mysql.connector.connect(user='root', 
-                              password='mauFJcuf5dhRMQrjj',
-                              host='172.18.0.8', 
-                              database='mydb')
+							  password='mauFJcuf5dhRMQrjj',
+							  host='172.18.0.8', 
+							  database='mydb')
 
 # Define a function to write each batch of streaming data to MySQL
 def write_to_mysql(batch_df, batch_id):
-    cursor = cnx.cursor()
+	collected = batch_df.collect()
 
-    # Convert the batch DataFrame to a list of tuples
-    data = [tuple(row) for row in batch_df.collect()]
+	# Send results to MySQL
+	print(collected)
 
-    # Construct the SQL query to insert the data into MySQL
-    query = "INSERT INTO mytable (col1, col2) VALUES (%s, %s)"
+	# To do: Call ML model on streaming data
 
-    # Insert the data into MySQL using a prepared statement
-    cursor.executemany(query, data)
-    cnx.commit()
+	# To do: Collect results from ML Model on streaming data
 
+	# To do: Send results to MySQL using the following code:
+
+	# cursor = cnx.cursor()
+
+	# # Convert the batch DataFrame to a list of tuples
+	# data = [tuple(row) for row in batch_df.collect()]
+
+	# # Construct the SQL query to insert the data into MySQL
+	# query = "INSERT INTO mytable (col1, col2) VALUES (%s, %s)"
+
+	# # Insert the data into MySQL using a prepared statement
+	# cursor.executemany(query, data)
+	# cnx.commit()
 
 def main():
-    spark = build_spark_session()
+	spark = build_spark_session()
 
-    """Get arguments from command line"""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--model-call-topic', 
-                        type=str,
-                        help='Name of the Kafka topic to receive machine learning call.')
+	"""Get arguments from command line"""
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument('--model-call-topic', 
+						type=str,
+						help='Name of the Kafka topic to receive machine learning call.')
 
-    parser.add_argument('--model-response-topic', 
-                        type=str,
-                        help='Name of the Kafka topic to send the response to the call.')
-    
-    args = parser.parse_args()
+	parser.add_argument('--model-response-topic', 
+						type=str,
+						help='Name of the Kafka topic to send the response to the call.')
+	
+	args = parser.parse_args()
 
-    """Create producer and consumer and interact with kafka"""
-    consumer_conf = get_consumer_config()
-    producer_conf = get_producer_config()
-    producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
-    del producer_conf['bootstrap.servers']
+	"""Create producer and consumer and interact with kafka"""
+	consumer_conf = get_consumer_config()
+	producer_conf = get_producer_config()
+	producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
+	del producer_conf['bootstrap.servers']
 
-    def preprocess_and_send_to_kafka(batch_df, batch_id):
-        """The function is fed into foreachBatch of a writestream.
-        It preprocesses the data and sends it to Kafka.
-        Thus it should be modified for necessary preprocessing"""
+	def msg_process(server, topic):
+		"""Create a streaming dataframe that takes in values of messages received, 
+		together with the current timestamp.
+		Then, print them out.
+		Then, process the message in batch
+		Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
+		base_df = (spark.
+				readStream
+				.format("kafka")
+				.option("kafka.bootstrap.servers", server)
+				.option("subscribe", topic)
+				.option("startingOffsets", "latest")
+				.load()
+			)
+		
+		# Perform some preprocessing
+		base_df = base_df.withColumn("key",col("key").cast("string"))
+		base_df = base_df.withColumn("patientid",split(col("key"), '_').getItem(0)) #get patient ID
+		base_df = base_df.withColumn("channel",split(col("key"), '_').getItem(1)) #get signal name
+		base_df = base_df.withColumn("parsed",from_json(col("value").cast("string"), "array<double>"))
 
-        # Preprocess the data
-        # Here it does nothing.
-        preprocessed_df = batch_df #.select(lower(col("value")).alias("value_lower"))
-        
-        # Send the preprocessed data to Kafka
-        preprocessed_df.write.format("kafka")\
-            .options(**producer_conf)\
-            .option("topic", "response-stream")\
-            .option("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .option("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .save()
+		# In the test case, each window has a duration of 5 seconds. The interval between each window is 2 seconds interval
+		# In the actual case, each window has a duration of 10 minutes. The interval between each window is 1 minute interval.
+		# The result from the following is suitable for machine learning inference. 
+		# This is because each row of the dataframe will contain all the necessary signals for one machine learning input.
+		base_df = base_df.withWatermark("timestamp", "3 seconds") \
+		.groupBy(
+			base_df.patientid,
+			window("timestamp", "5 seconds", '2 seconds')) \
+		.agg(to_json(struct(first("channel").alias("channel"),collect_list("parsed").alias("lst"))).alias("value2")) \
+			.selectExpr(
+			"patientid",
+			"window.start as start_time",
+			"window.end as end_time",
+			"value2"
+		)
 
-    def msg_process(server, topic):
-        """Create a streaming dataframe that takes in values of messages received, 
-        together with the current timestamp.
-        Then, print them out.
-        Then, process the message in batch
-        Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
+		# Temporarily, to demonstrate that our model can indeed be run, 
+		# We will use hard-coded waveform data in the form of a CSV.
+		# This is because we have yet to connect the result from hte processStream to the machine learning model.
+		# Once we manage to set up the actual data, 
+		# we will pull data from Kafka stream instead of having to retrieve data from local.
 
-        df = (spark.
-                readStream
-                .format("kafka")
-                .option("kafka.bootstrap.servers", server)
-                .option("subscribe", topic)
-                .option("startingOffsets", "latest")
-                .load()
-            )
-        
-        # Select the value and timestamp (the message is received)
-        base_df = df.selectExpr("CAST(key as STRING) as key", "CAST(value as STRING)", "timestamp")
-        
+		# Retrieve the model
+		model = torch.load(cfg['MODELPATH'])
+		device = torch.device("cuda" if cfg['USE_CUDA'] and torch.cuda.is_available() else "cpu")
+		data_df = pd.read_csv((cfg['EXPLOREPATH']+'/X.TESTINPUT'))
 
-        # The model needs to be called to store the data
-        model = torch.load(cfg['MODELPATH'])
-        device = torch.device("cuda" if cfg['USE_CUDA'] and torch.cuda.is_available() else "cpu")
+		# We will also be retrieving the age from the patient_id. 
+		age = 60
 
-        # Once we manage to set up the actual data, 
-        # we will pull data from Kafka stream instead of having to retrieve 
-        # data from local.
-        data_df = pd.read_csv((cfg['EXPLOREPATH']+'/X.TESTINPUT'))
+		y_pred, y_prob = run_model(model, device, data_df, age)
 
-        # We will also be retrieving the age from the patient_id
-        age = 60
+		print("Model has successfully run!")
+		print(y_pred)
+		print(y_prob)
+		
+		# Write the streaming data to MySQL using foreachBatch
+		# query = base_df.writeStream.foreachBatch(write_to_mysql).trigger(processingTime='10 seconds').start()
+		
+		query = base_df.writeStream.outputMode("append").format("console").option("truncate", "false").trigger(processingTime='10 seconds').start()
+		query.awaitTermination()
 
-        #y_pred, y_prob = run_model(model, device, data_df, age)
+		# # Create the first cursor for executing queries on the 'mytable' table
+		# cursor1 = cnx.cursor()
+		# query1 = 'SELECT * FROM mytable'
+		# cursor1.execute(query1)
+		# rows1 = cursor1.fetchall()
+		# print('Rows from mytable:')
+		# for idx, row in enumerate(rows1):
+		# 	# Too verbose
+		# 	if idx %50==0:
+		# 		print(row)
+		# 	idx+=1
+			
+		query.awaitTermination()
 
-        ## to see what "base_df" is like in the stream,
-        ## Uncomment base_df.writeStream.outputMode(...)
-        ## and comment out base_df.writeStream.foreachBatch(...)
-        query = base_df.writeStream.outputMode("append").format("console").trigger(processingTime='10 seconds').start()
-        query.awaitTermination()
+		return query
 
-        ## Write the preprocessed DataFrame to Kafka in batches.
-        # kafka_writer: DataStreamWriter = base_df.writeStream.foreachBatch(preprocess_and_send_to_kafka)
-        # kafka_query: StreamingQuery = kafka_writer.start()
-        # kafka_query.awaitTermination()
-        """
-        # Write the streaming data to MySQL using foreachBatch
-        query = base_df.writeStream.foreachBatch(write_to_mysql).trigger(processingTime='10 seconds').start()
-        # query.awaitTermination()
-        
-        for i in range(10):
-            print("Model has successfully run!")
-            print(y_pred)
-            print(y_prob)
-        
-        # Store the results in y_pred and y_prob
-        # run_model_dummy()
+	query = msg_process(consumer_conf['bootstrap.servers'], args.model_call_topic)
 
-        # Create the first cursor for executing queries on the 'mytable' table
-        cursor1 = cnx.cursor()
-        query1 = 'SELECT * FROM mytable'
-        cursor1.execute(query1)
-        rows1 = cursor1.fetchall()
-        print('Rows from mytable:')
-        for idx, row in enumerate(rows1):
-            # Too verbose
-            if idx %50==0:
-                print(row)
-            idx+=1
-        
-        query.awaitTermination()
-
-        return query
-        """
-
-    query = msg_process(consumer_conf['bootstrap.servers'], args.model_call_topic)
 
 if __name__ == "__main__":
-    #print("ML module!")
-    main()
+	#print("ML module!")
+	main()
--- bin/processStream.py
+++ bin/processStream.py
@@ -9,123 +9,125 @@ from utils import acked, get_consumer_config, get_producer_config, build_spark_s
 cfg = get_global_config()
 
 def main():
-    """Create SparkSession.
-    Explanation on why the .config(spark.jars.packages) is needed: 
-    https://stackoverflow.com/questions/72812187/pythonfailed-to-find-data-source-kafkav"""
-    spark = build_spark_session()
+	"""Create SparkSession.
+	Explanation on why the .config(spark.jars.packages) is needed: 
+	https://stackoverflow.com/questions/72812187/pythonfailed-to-find-data-source-kafkav"""
+	spark = build_spark_session()
 
-    """Get arguments from command line"""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--signal-list', 
-                        type=str,
-                        nargs="*",
-                        default=list(map(lambda x:x.replace(" ","_"), cfg["CHANNEL_NAMES"])),
-                        help='List of the Kafka topic to receive unprocessed data.')
-    parser.add_argument('--model-call-topic', 
-                        type=str, 
-                        help='Name of the Kafka topic to send preprocessed data to machine learning model.')
-    parser.add_argument('--model-response-topic', 
-                        type=str, 
-                        help='Name of the Kafka topic to receive response from machine learning model.')
+	"""Get arguments from command line"""
+	parser = argparse.ArgumentParser(description=__doc__)
+	parser.add_argument('--signal-list', 
+						type=str,
+						nargs="*",
+						default=list(map(lambda x:x.replace(" ","_"), cfg["CHANNEL_NAMES"])),
+						help='List of the Kafka topic to receive unprocessed data.')
+	parser.add_argument('--model-call-topic', 
+						type=str, 
+						help='Name of the Kafka topic to send preprocessed data to machine learning model.')
+	parser.add_argument('--model-response-topic', 
+						type=str, 
+						help='Name of the Kafka topic to receive response from machine learning model.')
 
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    """Create producer and consumer and interact with kafka"""
-    # Define Kafka consumer configuration
-    consumer_conf = get_consumer_config()
+	"""Create producer and consumer and interact with kafka"""
+	# Define Kafka consumer configuration
+	consumer_conf = get_consumer_config()
 
-    # Define Kafka producer configuration
-    producer_conf = get_producer_config()
-    producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
-    del producer_conf['bootstrap.servers']
+	# Define Kafka producer configuration
+	producer_conf = get_producer_config()
+	producer_conf['kafka.bootstrap.servers'] = producer_conf['bootstrap.servers']
+	del producer_conf['bootstrap.servers']
 
-    # Define the function to preprocess and send the data to Kafka
-    def preprocess_and_send_to_kafka(batch_df, batch_id):
-        """The function is fed into foreachBatch of a writestream.
-        It preprocesses the data and sends it to Kafka.
-        Thus it should be modified for necessary preprocessing"""
+	# Define the function to preprocess and send the data to Kafka
+	def preprocess_and_send_to_kafka(batch_df, batch_id):
+		"""The function is fed into foreachBatch of a writestream.
+		It preprocesses the data and sends it to Kafka.
+		Thus it should be modified for necessary preprocessing"""
 
-        # select the data
-        # value should be string
-        # preprocessed_df = batch_df.selectExpr("CAST(average as STRING) as value")
+		# select the data
+		# value should be string
+		# preprocessed_df = batch_df.selectExpr("CAST(average as STRING) as value")
 
-        
-        preprocessed_df = batch_df.groupby("key","start","end")\
-            .agg(to_json(collect_list(struct("channel","average"))).alias("value")) \
-            .selectExpr(
-            'key',
-            'value'
-        )
-        
-        
-        # Send the preprocessed data to Kafka
-        preprocessed_df.write.format("kafka")\
-            .options(**producer_conf)\
-            .option("topic", "call-stream")\
-            .option("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .option("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
-            .save()
+		# Create a new key in the format of "patientid_channelname". 
+		# An example of the new key is "p000194_SpO2"
+		# The "value" here will be an array containing one value of SpO2 of the patient.
+		preprocessed_df = batch_df.groupby("key","channel","start","end")\
+			.agg(to_json(collect_list("average")).alias("value")) \
+			.selectExpr(
+			'concat(key, "_", channel) as key',
+			'value'
+		)
 
-    def msg_process(server, topics):
-        """Create a streaming dataframe that takes in values of messages received, 
-        together with the current timestamp.
-        Then, print them out.
-        Then, process the message in batch
-        Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
+		# print out preprocessed_df
+		preprocessed_df.show()
+		
+		# Send the preprocessed data to Kafka
+		preprocessed_df.write.format("kafka")\
+			.options(**producer_conf)\
+			.option("topic", "call-stream")\
+			.option("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
+			.option("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")\
+			.save()
 
-        df = (spark.
-                readStream
-                .format("kafka")
-                .option("kafka.bootstrap.servers", server)
-                .option("subscribe", ",".join(topics))
-                .option("startingOffsets", "latest")
-                .load()
-            )
-        
-        # Select the value and timestamp (the message is received)
+	def msg_process(server, topics):
+		"""Create a streaming dataframe that takes in values of messages received, 
+		together with the current timestamp.
+		Then, print them out.
+		Then, process the message in batch
+		Reference link: https://medium.com/@aman.parmar17/handling-real-time-kafka-data-streams-using-pyspark-8b6616a3a084"""
 
-        base_df = df.selectExpr("CAST(key as STRING) as key",
-                                "CAST(replace(substring_index(CAST(value as STRING), ',' ,1),'[','') as STRING) as channel",
-                                "CAST(replace(substring_index(CAST(value as STRING), ',' ,-1),']','') as FLOAT) as value",
-                                "date_format(timestamp,'HH:mm:ss') as time",
-                                "CAST(timestamp as TIMESTAMP) as timestamp")\
-                    .fillna(0) 
-        
-        # low-pass filtering over l=3 mins
-        base_df = base_df.withWatermark("timestamp", "3 minutes") \
-        .groupBy(
-            base_df.key,
-            base_df.channel,
-            window("timestamp", "3 minutes", '5 seconds')) \
-        .agg(avg("value").alias("average")) \
-        .selectExpr(
-            "key"
-            ,"replace(channel, '\"', '') as channel"
-            ,"window.start"
-            ,"window.end"
-            ,'average'
-        )
-        
-        # to see what "base_df" is like in the stream,
-        # Uncomment base_df.writeStream.outputMode(...)
-        # and comment out base_df.writeStream.foreachBatch(...)
-        # query = base_df.writeStream.outputMode("append").format("console").trigger(processingTime='10 seconds').start()
-        # query.awaitTermination()
+		df = (spark.
+				readStream
+				.format("kafka")
+				.option("kafka.bootstrap.servers", server)
+				.option("subscribe", ",".join(topics))
+				.option("startingOffsets", "latest")
+				.load()
+			)
+		
+		# Select the value and timestamp (the message is received)
 
-        # Write the preprocessed DataFrame to Kafka in batches.
-        
-        kafka_writer: DataStreamWriter = base_df.writeStream \
-        .foreachBatch(preprocess_and_send_to_kafka)
+		base_df = df.selectExpr("CAST(key as STRING) as key",
+								"CAST(replace(substring_index(CAST(value as STRING), ',' ,1),'[','') as STRING) as channel",
+								"CAST(replace(substring_index(CAST(value as STRING), ',' ,-1),']','') as FLOAT) as value",
+								"date_format(timestamp,'HH:mm:ss') as time",
+								"CAST(timestamp as TIMESTAMP) as timestamp")\
+					.fillna(0) 
 
-        kafka_query: StreamingQuery = kafka_writer.start()
-        
-        print("await termination")
-        kafka_query.awaitTermination()
-        
+		# low-pass filtering over l=3 mins 
+		# In the actual case, each window has a duration of 3 minutes. The interval between each window is 5 seconds interval.
+		# In the test case, each window has a duration of 5 seconds. The interval between each window is 2 seconds interval
+		base_df = base_df.withWatermark("timestamp", "3 seconds") \
+		.groupBy(
+			base_df.key,
+			base_df.channel,
+			window("timestamp", "5 seconds", '2 seconds')) \
+		.agg(avg("value").alias("average")) \
+		.selectExpr(
+			"key"
+			,"replace(channel, '\"', '') as channel"
+			,"window.start"
+			,"window.end"
+			,'average'
+		)
+		
+		# To see what "base_df" is like in the stream,
+		# Uncomment base_df.writeStream.outputMode(...)
+		# and comment out base_df.writeStream.foreachBatch(...)
+		# query = base_df.writeStream.outputMode("append").format("console").option("truncate", "false").trigger(processingTime='10 seconds').start()
+		# query.awaitTermination()
 
-    msg_process(consumer_conf['bootstrap.servers'],
-                args.signal_list)
+		# Write the preprocessed DataFrame to Kafka in batches.
+		kafka_writer: DataStreamWriter = base_df.writeStream.foreachBatch(preprocess_and_send_to_kafka)
+
+		kafka_query: StreamingQuery = kafka_writer.start()
+		
+		kafka_query.awaitTermination()
+
+	msg_process(consumer_conf['bootstrap.servers'],
+				args.signal_list)
 
 if __name__ == "__main__":
-    main()
+	main()
